94001e97fdeb41bc6f5540e4c22d0aac0e693635
James Chow <zhouxy.vortex@gmail.com>
2014-05-20 15:51:16 +0800

    更新hdfs配置文件，将hadoop版本降低至1.0+

--- modules/parent/install.sh
+++ modules/parent/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+echo "Install parent module to local repository."
+
+set MAVEN_OPTS=$MAVEN_OPTS -XX:MaxPermSize=128m
+
+# First step clean now target, second step install parent module pom to local repository.
+mvn clean install -Dmaven.test.skip=true
